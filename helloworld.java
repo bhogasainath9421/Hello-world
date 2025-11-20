100886e5dfb8ab6a56745b58cf529e200cbde4e4
EC2 Default User <ec2-user@ip-172-31-10-155.ap-south-1.compute.internal>
2025-11-20 10:38:43 +0000

    added sainath name

--- helloworld.java
+++ helloworld.java
@@ -1,14 +1,16 @@
 public class helloworld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
+        System.out.println("Hello, Sainath!");
+        System.out.println("Hello, Sainath!");
+        System.out.println("Hello, Sainath!");
+        System.out.println("Hello, Sainath!");
+        System.out.println("Hello, Sainath!");
+        System.out.println("Hello, Sainath!");
+        System.out.println("Hello, Sainath!");
+        System.out.println("Hello, Sainath!");
+        System.out.println("Hello, Sainath!");
+        System.out.println("Hello, Sainath!");
+        System.out.println("Hello, Sainath!");
+        System.out.println("Hello, Sainath!");
     }
 }
